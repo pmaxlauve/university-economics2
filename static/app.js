baac52ea927d1529dd2ae3d54cc6b41064abde9b
pmaxlauve <71564864+pmaxlauve@users.noreply.github.com>
2021-02-23 20:24:45 -0600

    first commit

---
+++ static/app.js
@@ -0,0 +1,49 @@
+const data = {
+    name: '2019 US Employment',
+    color: 'magenta',
+    children: [{
+      name: 'Employed',
+      color: 'yellow',
+      size: 232152,
+      children: [{
+        name: "Male",
+        color: "blue",
+        size: 120902
+      }, {
+        name: "Female",
+        color: "pink",
+        size: 111253
+
+
+      }
+    ]
+    },
+      {
+        name: 'Unemployed',
+        color: 'red',
+        children: [
+          {
+            name: "Male",
+            color: "blue",
+            size: 3358
+
+          }, {
+            name: "Female",
+            color: "pink",
+            size: 3243
+
+    
+    
+          }
+        
+        ]
+    }]
+    
+  };
+
+  Sunburst()
+    .data(data)
+    .size('size')
+    .color('color')
+    .radiusScaleExponent(1)
+    (document.getElementById('chart'));

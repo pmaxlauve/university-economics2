7c22edc3dcba174a326df5a8af4e211a4cfda70c
pmaxlauve <71564864+pmaxlauve@users.noreply.github.com>
2021-02-27 10:24:56 -0600

    split sunbursts

--- static/js/sunburstB.js
+++ static/js/sunburstB.js
@@ -30,153 +30,7 @@ var colors = ["#B4D8C9", "#3f3650", "#d65858",
 
 
 
-
-const data1 = {
-    name: '2019 US Employment by Gender',
-    color: colors[0],
-    children: [{
-      name: `Employed (${(100*_total[1].total/_total[0].total).toFixed(2)}%)`,
-      color: colors[1],
-      size: _total[1].total,
-      children: [{
-        name: `Male (${(100*_men[1].total/_total[0].total).toFixed(2)}%)`,
-        color: colors[3],
-        size: _men[1].total,
-        children: [{
-          name: `< HS Diploma (${(100*_men[1].less_hs/_total[0].total).toFixed(2)}%)`,
-          color: colors[9],
-          size: _men[1].less_hs
-        }, {
-          name: `HS Diploma (${(100*_men[1].hs_diploma/_total[0].total).toFixed(2)}%)`,
-          color: colors[10],
-          size: _men[1].hs_diploma
-        }, {
-          name: `Some College (${(100*_men[1].some_college/_total[0].total).toFixed(2)}%)`,
-          color: colors[11],
-          size: _men[1].some_college
-        }, {
-          name: `Associates (${(100*_men[1].associate/_total[0].total).toFixed(2)}%)`,
-          color: colors[12],
-          size: _men[1].associate
-        }, {
-          name: `Bachelors (${(100*_men[1].bachelor/_total[0].total).toFixed(2)}%)`,
-          color: colors[13],
-          size: _men[1].bachelor
-        }, {
-          name: `Advanced Degree (${(100*_men[1].adv_degree/_total[0].total).toFixed(2)}%)`,
-          color: colors[14],
-          size: _men[1].adv_degree
-        }]
-      }, {
-        name: `Female (${(100*_women[1].total/_total[0].total).toFixed(2)}%)`,
-        color: colors[4],
-        size: _women[1].total,
-        children: [{
-          name: `< HS Diploma (${(100*_women[1].less_hs/_total[0].total).toFixed(2)}%)`,
-          color: colors[9],
-          size: _women[1].less_hs
-        }, {
-          name: `HS Diploma (${(100*_women[1].hs_diploma/_total[0].total).toFixed(2)}%)`,
-          color: colors[10],
-          size: _women[1].hs_diploma
-        }, {
-          name: `Some College (${(100*_women[1].some_college/_total[0].total).toFixed(2)}%)`,
-          color: colors[11],
-          size: _women[1].some_college
-        }, {
-          name: `Associates (${(100*_women[1].associate/_total[0].total).toFixed(2)}%)`,
-          color: colors[12],
-          size: _women[1].associate
-        }, {
-          name: `Bachelors (${(100*_women[1].bachelor/_total[0].total).toFixed(2)}%)`,
-          color: colors[13],
-          size: _women[1].bachelor
-        }, {
-          name: `Advanced Degree (${(100*_women[1].adv_degree/_total[0].total).toFixed(2)}%)`,
-          color: colors[14],
-          size: _women[1].adv_degree
-        }]
-
-
-      }
-    ]
-    },
-      {
-        name: `Unemployed (${(100*_total[2].total/_total[0].total).toFixed(2)}%)`,
-        color: colors[2],
-        size: _total[2].total,
-        children: [
-          {
-            name: `Male (${(100*_men[2].total/_total[2].total).toFixed(2)}%)`,
-            color: colors[3],
-            size: _men[2].total,
-            children: [{
-              name: `< HS Diploma (${(100*_men[2].less_hs/_total[0].total).toFixed(2)}%)`,
-              color: colors[9],
-              size: _men[2].less_hs
-            }, {
-              name: `HS Diploma (${(100*_men[2].hs_diploma/_total[0].total).toFixed(2)}%)`,
-              color: colors[10],
-              size: _men[2].hs_diploma
-            }, {
-              name: `Some College (${(100*_men[2].some_college/_total[0].total).toFixed(2)}%)`,
-              color: colors[11],
-              size: _men[2].some_college
-            }, {
-              name: `Associates (${(100*_men[2].associate/_total[0].total).toFixed(2)}%)`,
-              color: colors[12],
-              size: _men[2].associate
-            }, {
-              name: `Bachelors (${(100*_men[2].bachelor/_total[0].total).toFixed(2)}%)`,
-              color: colors[13],
-              size: _men[2].bachelor
-            }, {
-              name: `Advanced Degree (${(100*_men[2].adv_degree/_total[0].total).toFixed(2)}%)`,
-              color: colors[14],
-              size: _men[2].adv_degree
-            }]
-
-          }, {
-            name: `Female (${(100*_women[2].total/_total[2].total).toFixed(2)}%)`,
-            color: colors[4],
-            size: _women[2].total,
-            children: [{
-              name: `< HS Diploma (${(100*_women[2].less_hs/_total[0].total).toFixed(2)}%)`,
-              color: colors[9],
-              size: _women[2].less_hs
-            }, {
-              name: `HS Diploma (${(100*_women[2].hs_diploma/_total[0].total).toFixed(2)}%)`,
-              color: colors[10],
-              size: _women[2].hs_diploma
-            }, {
-              name: `Some College (${(100*_women[2].some_college/_total[0].total).toFixed(2)}%)`,
-              color: colors[11],
-              size: _women[2].some_college
-            }, {
-              name: `Associates (${(100*_women[2].associate/_total[0].total).toFixed(2)}%)`,
-              color: colors[12],
-              size: _women[2].associate
-            }, {
-              name: `Bachelors (${(100*_women[2].bachelor/_total[0].total).toFixed(2)}%)`,
-              color: colors[13],
-              size: _women[2].bachelor
-            }, {
-              name: `Advanced Degree (${(100*_women[2].adv_degree/_total[0].total).toFixed(2)}%)`,
-              color: colors[14],
-              size: _women[2].adv_degree
-            }]
-
-    
-    
-          }
-        
-        ]
-    }]
-    
-  };
-
-
-const data2 = {
+const data = {
   name: '2019 US Employment by Ethnicity',
   color: colors[0],
   children: [{
@@ -427,16 +281,11 @@ const data2 = {
 
 
 Sunburst()
-    .data(data1)
+    .data(data)
     .size('size')
     .color('color')
     .radiusScaleExponent(1.5)
-    (document.getElementById('chart1'));
+    (document.getElementById('chart'));
 
-Sunburst()
-    .data(data2)
-    .size('size')
-    .color('color')
-    .radiusScaleExponent(1.5)
-    (document.getElementById('chart2'));
+
     
